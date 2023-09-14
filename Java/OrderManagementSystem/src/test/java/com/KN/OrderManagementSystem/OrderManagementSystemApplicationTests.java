cd6d2bbb04c1b616cfc46b23af928f389934458b
Walther Kraam <wkraam@yahoo.com>
2023-09-14 17:19:17 +0300

    Added The Order Management System task
    Added the order management system api task

---
+++ Java/OrderManagementSystem/src/test/java/com/KN/OrderManagementSystem/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KN.OrderManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
